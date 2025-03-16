4b8132bd67032bd8547500596819046be46fb315
madhul91 <madhuldubey06@gmail.com>
2025-03-16 22:21:15 +0530

    add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/send/mail",
+        "https://backend-gym-799h.onrender.com",
         {
           name,
           email,
